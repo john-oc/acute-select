51998f4e3a785762668f589a3ea31b292f840873
john-oc <john@precisionit.co.uk>
2014-11-24 23:02:09 +0000

    Added test web service missed on previous commit.

---
+++ acute/test-pages/TestWS.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="TestWS.asmx.cs" Class="acute.test_pages.TestWS" %>
---
+++ acute/test-pages/TestWS.asmx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Services;
+
+namespace acute.test_pages
+{
+    /// <summary>
+    /// Summary description for TestWS
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    [System.Web.Script.Services.ScriptService]
+    public class TestWS : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public List<string> GetItemData()
+        {
+            Thread.Sleep(500);
+
+            return new List<string>
+            {
+                "Take",
+                "Your",
+                "Pick"
+            };
+        }
+    }
+}
